660453cb8db9189df91a985e0aa03994db542695
set <set>
2022-02-19 11:12:19 -0800

    Audio Object untested, need to implement selectTime

--- src/main/java/com/nocrusts/sonido/SoundObject.java
+++ src/main/java/com/nocrusts/sonido/SoundObject.java
@@ -4,20 +4,49 @@ import java.io.*;
 import javax.sound.sampled.*;
 
 public class SoundObject {
-    private File fileLoc;
     private Clip audioClip;
+    private AudioInputStream audioIn;
 
-    public SoundObject(final String fileLocStr) {
+    public SoundObject(String fileLoc) {
+        setSoundFile(fileLoc);
+    }
+
+    public void playSound() {
+        audioClip.start();
+    }
+
+    public void stopSound() {
+        audioClip.stop();
+    }
+
+    public void selectTime(int SecondsIn) {
+        //TODO: be implemented
+    }
+
+    public void setSoundFile (String fileLoc) {
+        File file = new File("/" + fileLoc);
         try {
-            fileLoc = new File(fileLocStr);
-            AudioInputStream audioFile = AudioSystem.getAudioInputStream(fileLoc);
-            //TODO: Finish audio implementation
-        } catch (UnsupportedAudioFileException e) {
-            System.out.println("Unsupported File.");
+            audioIn = AudioSystem.getAudioInputStream(file);
         } catch (IOException e) {
-            System.out.println("General IO error.");
+            System.out.println("File not found!");
+        } catch (UnsupportedAudioFileException e) {
+            System.out.println("Sorry, unsupported audio format!");
         }
 
+        try {
+            audioClip = AudioSystem.getClip();
+        } catch (LineUnavailableException e) {
+            System.out.println("Another program is already using the line.");
+        }
 
+        try {
+            audioClip.open(audioIn);
+        } catch (LineUnavailableException e) {
+            System.out.println("Another program is already using the line.");
+        } catch (IOException e) {
+            System.out.println("File not found!");
+        }
     }
+
+
 }
